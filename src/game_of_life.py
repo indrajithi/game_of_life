4561d7d2cc16ec800349910b241cba09b155aba1
Indrajith Indraprastham <indrajith.indraprastham@go-jek.com>
2021-06-27 10:45:03 +0530

    Game of life class which finds next generation of life in the matrix

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,3 +1,68 @@
-class GameOfLife:
-    def __int__(self):
-      pass
+from grid import Grid
+
+class GameOfLife(Grid):
+    def __init__(self):
+      Grid.__init__(self)
+      self.matrix = self.glider()
+      
+    def is_cord_positive(self, cord):
+      return cord[0] >= 0 and cord[1] >= 0
+    
+    def is_below_last_cord(self, cord):
+      return cord[0] < self._rows and cord[1] < self._columns
+         
+    def is_life_in_grid(self, cord):
+      return self.is_cord_positive(cord) and self.is_below_last_cord(cord)
+    
+      
+    def get_neighbors(self, row, col):
+      neighbors = [(row - 1, col -1), (row - 1, col), (row - 1, col + 1),
+                    (row, col - 1), (row, col + 1),
+                    (row + 1, col - 1), (row + 1, col), (row + 1, col + 1) ]
+
+
+      return neighbors
+    
+    def set_matrix_in_grid(self, matrix):
+      self.matrix = [life for life in matrix if self.is_life_in_grid(life)]
+    
+    def get_alive_neighbors(self, life):
+      return [n for n in self.get_neighbors(*life) if n in self.matrix]
+    
+    def is_alive(self, life, neighbors):
+      if len(neighbors) < 2: # under population
+        return False
+      if len(neighbors) > 3: # over population
+        return False
+      return True
+    
+    def reproduction(self, neighborhood):
+      births = []
+      for life in neighborhood:
+        a = self.get_alive_neighbors(life)
+        if len(self.get_alive_neighbors(life)) == 3:
+         births.append(life)
+      return births
+      
+    def next_generation(self):
+      _matrix = []
+      neighborhood = set(self.matrix)
+      for life in self.matrix:
+        neighbors = self.get_neighbors(*life)
+        alive_neighbors = [n for n in neighbors if n in self.matrix]
+        neighborhood.update(neighbors)
+        
+        if self.is_alive(life, alive_neighbors):
+          _matrix.append(life)
+        
+      _matrix += self.reproduction(neighborhood)
+      self.set_matrix_in_grid(_matrix)
+      
+
+
+game = GameOfLife()
+# game.run()
+print(game.next_generation())
+game.run(game.setup)
+game.draw(game.matrix)
+game.display(3000)
--- src/grid.py
+++ src/grid.py
@@ -3,28 +3,37 @@ import curses
 
 class Grid:
   def __init__(self):
-    self.rows, self.columns = map(int, subprocess.check_output(['stty', 'size']).split())
-    self.marker = u"\u2584"
+    self._rows, self._columns = map(int, subprocess.check_output(['stty', 'size']).split())
+    self._marker = u"\u2584"
     self._stdscr = None
-    
+    curses.wrapper(self.setup)
+
+  
   def setup(self, stdscr):
     self._stdscr = stdscr
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
     stdscr.border(0)
+  
+  def glider(self):
+    middle_row = int(self._rows/2)
+    middle_column = int(self._columns/2)
+
+    return [(middle_row, middle_column),
+            (middle_row + 1, middle_column + 1),
+            (middle_row + 2, middle_column - 1), (middle_row + 2, middle_column), (middle_row + 2, middle_column + 1)]
     
   def draw_a_glider(self):
-    middle_row = int(self.rows/2)
-    middle_column = int(self.columns/2)
-    coordinates = [(middle_row, middle_column),
-                   (middle_row + 1, middle_column + 1),
-                   (middle_row + 2, middle_column - 1), (middle_row + 2, middle_column), (middle_row + 2, middle_column + 1)]
+    coordinates = self.glider()
     
     for coordinate in coordinates:
-      print(coordinates)
-      self.insert_charactor(coordinate[0], coordinate[1], self.marker)
-    self.display(2000)
+      self.insert_charactor(coordinate[0], coordinate[1], self._marker)
+    self.display(1000)
+  
+  def draw(self, coordinates):
+    for coordinate in coordinates:
+      self.insert_charactor(coordinate[0], coordinate[1], self._marker)
 
   def display(self, m_seconds):
       self._stdscr.refresh()
@@ -37,9 +46,9 @@ class Grid:
   def insert_charactor(self, row, column, character):
     self._stdscr.addstr(row, column, character)
     
-  
-  def run(self):
-    curses.wrapper(self.main)
+
+  def run(self, func):
+    curses.wrapper(func)
 
 if __name__ == "__main__":
   Grid().run()
